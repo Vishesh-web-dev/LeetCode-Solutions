c6b0d530f19f3a5171929d5da70f21e739ec91f0
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2023-09-11 16:24:47 +0530

    Time: 7 ms (87.01%) | Memory: 12.5 MB (96.45%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         //brute
+        unordered_map<int,vector<int>> mpp;
         vector<vector<int>> ans;
-        vector<pair<int,int>> res;
         for(int i = 0 ; i < groupSizes.size() ; i++){
-            pair<int,int> p = make_pair(groupSizes[i],i);
-            res.push_back(p);
-        }
-        sort(res.begin(),res.end());
-
-        for(int i = 0 ; i < res.size() ;){
-            vector<int> temp;
-            int times = res[i].first;
-            while(times--){
-                temp.push_back(res[i].second);
-                i++;
+            int sz = groupSizes[i];
+            mpp[sz].push_back(i);
+            if(mpp[sz].size() == sz){
+                ans.push_back(mpp[sz]);
+                mpp[sz].clear();
             }
-            ans.push_back(temp);
         }
+        
         return ans;
     }
 };
