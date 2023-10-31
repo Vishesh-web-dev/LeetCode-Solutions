29c9a090d5ec430669d62faee258c9b991f44e23
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2023-10-31 08:17:54 +0530

    Time: 77 ms (90.05%) | Memory: 76.3 MB (69.22%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        //in O(1) space need to store prev value
         int prev = pref[0];
         for(int i = 1 ; i < pref.size() ; i++){
             int temp = pref[i];
-            pref[i] = pref[i]^prev;
+            pref[i] ^= prev;
             prev = temp;
         }
         return pref;
