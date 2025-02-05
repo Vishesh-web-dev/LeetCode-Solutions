0982c738b4cc5f4ced07ee44fb1b10f803c37d91
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-02-05 15:35:45 +0530

    Time: 27 ms (98.48%) | Memory: 18.3 MB (96.71%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    int encode(char a){
+        switch(a){
+            case 'A': return 0;
+            case 'C': return 1;
+            case 'G': return 2;
+            case 'T': return 3;
+        }
+        return -1;
+    }
     vector<string> findRepeatedDnaSequences(string s) {
         // s.substr(i, j - i + 1)
         vector<string> res;
@@ -7,19 +16,39 @@ public:
         if (sz < 10) {
             return res;
         }
+        unordered_map<int, int> mpp;
 
-        map<string, int> mpp;
-        int i = 0, j = 9;
-        while (j < sz) {
-            string sub = s.substr(i, j - i + 1);
-            mpp[sub]++;
+        // while (j < sz) {
+        //     string sub = s.substr(i, j - i + 1);
+        //     mpp[sub]++;
 
-            if (mpp[sub] == 2) {
-                res.push_back(sub);
-            }
-            i++;
-            j++;
+        //     if (mpp[sub] == 2) {
+        //         res.push_back(sub);
+        //     }
+        //     i++;
+        //     j++;
+        // }
+        // return res;
+
+
+        //above solution takes more space so bit encodein is used
+        int mask = 0;
+        for(int i = 0; i< 10; i++){
+            mask = ((mask << 2) | encode(s[i]));
         }
+        int ones = (1 << 20)-1;
+        mpp[mask]++;
+        int i = 1,j=10;
+        while(j < sz){
+            mask = (((mask << 2) | encode(s[j]))& ones) | (encode(s[i]) << 18); 
+            mpp[mask]++;
+            if(mpp[mask] == 2){
+                res.push_back(s.substr(i, j - i + 1));
+            }
+            j++;
+            i++;
+        }
+        
         return res;
     }
 };
