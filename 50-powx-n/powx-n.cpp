738ae0e9dce2e2d253ae16a4e453a11bb563f86e
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-02-07 00:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.68%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    // double calc(double x, int n) {}
+    double myPow(double x, int n) {
+        bool isNeg = false;
+        long long exp = n;
+        if (exp < 0) {
+            isNeg = true;
+            exp = exp*-1;
+        }
+
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return isNeg ? 1/x: x;
+        } else if (n & 1) {
+            double res = x * myPow(x, exp - 1);
+            return isNeg ? 1/res: res;
+        } else {
+            double res = myPow(x * x, exp / 2);
+            return isNeg ? 1/res: res;
+        }
+    }
+};
