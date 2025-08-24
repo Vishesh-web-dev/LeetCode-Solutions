0322e09db7e3c427034e0586d09899aeb86aff72
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-08-24 08:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 60.1 MB (59.08%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -8,23 +8,19 @@ public:
         // currCnt = 0
 
         int maxCnt = 0, prevCnt = 0, currCnt = 0;
-        bool isAllOne = true;
+
         for (auto i : nums) {
             if (i != 1) {
                 maxCnt = max(maxCnt, prevCnt + currCnt);
                 prevCnt = currCnt;
                 currCnt = 0;
-                isAllOne = false;
             } else {
                 currCnt++;
             }
         }
-        if (isAllOne) {
-            maxCnt = currCnt - 1;
-        } else {
-            maxCnt = max(maxCnt, prevCnt + currCnt);
-        }
-        
-        return maxCnt;
+
+        maxCnt = max(maxCnt, prevCnt + currCnt);
+
+        return maxCnt == nums.size() ? maxCnt - 1 : maxCnt; //remove 1 when all ones
     }
 };
