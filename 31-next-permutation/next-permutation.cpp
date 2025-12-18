85c71ecb56170025d50118e14c759cc206406564
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-12-18 15:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (91.72%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int smallestToIncrease = 101, smallestToIncIdx;
-        //lets figure out smallest group from left that can be increased first
+        //lets figure out smallest group from left that can be increased first or pivot
         int groupSize = 2;
         for(int i = nums.size() -1; i > 0 ;i--){
             if(nums[i] <= nums[i-1]){
@@ -11,7 +11,7 @@ public:
                 break;
             }
         }
-        cout << groupSize <<endl;
+
         if(groupSize > nums.size()){
             reverse(nums.begin(), nums.end());
             return;
@@ -22,9 +22,9 @@ public:
         //now find just greater or smallest number which is greater than n-groupSize index element
 
         for(int i =( nums.size() - groupSize + 1); i < nums.size() ; i++){
-            if(nums[i] > nums[nums.size() - groupSize] && nums[i] < smallestToIncrease){
+            if(nums[i] > nums[nums.size() - groupSize] && nums[i] <= smallestToIncrease){
                 smallestToIncrease = nums[i];
-                smallestToIncIdx = i;
+                smallestToIncIdx = i; //last index for reverse thing to work
             }
         }
 
@@ -33,7 +33,13 @@ public:
         nums[nums.size() - groupSize] = nums[smallestToIncIdx];
         nums[smallestToIncIdx] = temp;
 
+        //avoid it since everything after pivot is in decresing order 
+        //so first swap just likw we did avobe and reverse after pivot instead of sort
+
+        //1.
         //now sort all numbers after nums.size() - groupSize
-        sort(nums.begin() + nums.size() - groupSize + 1, nums.end());
+        // sort(nums.begin() + nums.size() - groupSize + 1, nums.end());
+
+         reverse(nums.begin() + nums.size() - groupSize + 1, nums.end());
     }
 };
