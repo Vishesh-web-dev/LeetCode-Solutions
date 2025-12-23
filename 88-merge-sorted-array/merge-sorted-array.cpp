821018439affe673dde7d7f560e7078962fd59d7
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-12-23 11:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (38.71%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1 = m-1, p2 = n-1, p3 = m+n-1;
-        while(p1 >= 0 && p2 >= 0){
-            if(nums1[p1] < nums2[p2]){
-                nums1[p3--] = nums2[p2--];
-            }else{
-                nums1[p3--] = nums1[p1--];
+        // have three pointers and start from back to overwrite 0 correctly
+        int st1 = m - 1, st2 = n - 1, en = m + n - 1;
+
+        while (st1 >= 0 && st2 >= 0) {
+            if (nums1[st1] < nums2[st2]) {
+                nums1[en--] = nums2[st2--];
+            } else {
+                nums1[en--] = nums1[st1--];
             }
         }
-        while(p2 >= 0){
-            nums1[p3--] = nums2[p2--];
+
+        while (st1 >= 0) {
+            nums1[en--] = nums1[st1--];
         }
-        while(p1 >= 0){
-            nums1[p3--] = nums1[p1--];
+        while (st2 >= 0) {
+            nums1[en--] = nums2[st2--];
         }
     }
 };
