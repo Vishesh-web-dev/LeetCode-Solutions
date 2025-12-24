985d9145b3f50c2af30d949b2a3fc67fbf2df470
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-12-24 14:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (45.84%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        // brute
         int totalApples = accumulate(apple.begin(), apple.end(), 0);
-        sort(capacity.begin(), capacity.end(), greater<int>());
+        // sort(capacity.begin(), capacity.end()); //1 way
 
-        int boxesNeeded = 0, capacityItr = 0;
+        // counting sort  O(n) ==> use when sorting is required but n and //2nd
+        // way nums[n] is small
+        vector<int> cntArr(51, 0);
+        for (int capacity : capacity) {
+            cntArr[capacity]++;
+        }
+        int index = 0;
+        for (int i = 0; i < cntArr.size(); i++) {
+            while (cntArr[i] != 0) {
+                capacity[index++] = i;
+                cntArr[i]--;
+            }
+        }
+        
+        int boxesNeeded = 0, capacityItr = capacity.size() - 1;
 
-        while (capacityItr < capacity.size() && totalApples > 0) {
-            totalApples -= capacity[capacityItr++];
+        while (capacityItr >= 0 && totalApples > 0) {
+            totalApples -= capacity[capacityItr--];
             boxesNeeded++;
         }
 
