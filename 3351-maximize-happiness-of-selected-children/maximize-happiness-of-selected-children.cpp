f904fe08ca5061a404a4482b635c666ddbaa9624
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2025-12-26 11:24:49 +0530

    Time: 154 ms (66.78%) | Memory: 108 MB (48.17%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        // brute solution
+        //solution
         sort(happiness.begin(), happiness.end(), greater<int>());
         long long totalHappiness = 0;
         for (int i = 0; i < k; i++) {
             if ((happiness[i] - i) > 0) {
-
                 totalHappiness += happiness[i] - i;
             }
         }
         return totalHappiness;
+
+//         PQ wins when:
+
+// Values change irregularly
+
+// You don’t know order in advance
+
+// Operations are interleaved (streaming data)
+
+// Big-O ignores how expensive each step is.
+
+// Sorting
+
+// Happens once
+
+// Very optimized (C++ sort = introsort)
+
+// Mostly sequential memory access (cache-friendly)
+
+// Priority Queue
+
+// Each operation:
+
+// Pointer jumps
+
+// Heap rebalancing
+
+// Multiple comparisons
     }
 };
