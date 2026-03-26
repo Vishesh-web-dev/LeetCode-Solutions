f7b01c461eb644c57a8342c44b76535bc974ac50
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2026-03-26 10:47:08 +0530

    Time: 8 ms (66.30%) | Memory: 130.2 MB (53.26%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -2,47 +2,35 @@ class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<long long>> prefix(m, vector<long long>(n, 0));
-        vector<vector<long long>> suffix(m, vector<long long>(n, 0));
+        vector<long long> prefixH(m, 0);
+        vector<long long> prefixV(n, 0);
+        long long total = 0;
 
         for (int i = 0; i < m; i++) {
+            long long sum = 0;
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    prefix[0][0] = grid[0][0];
-                } else if (i == 0) {
-                    prefix[i][j] = grid[i][j] + prefix[i][j - 1];
-                } else if (j == 0) {
-                    prefix[i][j] = grid[i][j] + prefix[i - 1][j];
-                } else {
-                    prefix[i][j] = grid[i][j] + prefix[i][j - 1] +
-                                   prefix[i - 1][j] - prefix[i - 1][j - 1];
-                }
+                sum += grid[i][j];
+                total += grid[i][j];
             }
+            prefixH[i] = sum + (i == 0 ? 0 : prefixH[i - 1]);
         }
 
-         for (int i = m-1; i >= 0; i--) {
-            for (int j = n-1; j >= 0; j--) {
-                if (i == m-1 && j == n-1) {
-                    suffix[i][j] = grid[i][j];
-                } else if (i == m-1) {
-                    suffix[i][j] = grid[i][j] + suffix[i][j + 1];
-                } else if (j == n-1) {
-                    suffix[i][j] = grid[i][j] + suffix[i + 1][j];
-                } else {
-                    suffix[i][j] = grid[i][j] + suffix[i][j + 1] +
-                                   suffix[i + 1][j] - suffix[i + 1][j + 1];
-                }
+        for (int j = 0; j < n; j++) {
+            long long sum = 0;
+            for (int i = 0; i < m; i++) {
+                sum += grid[i][j];
             }
+            prefixV[j] = sum + (j == 0 ? 0 : prefixV[j - 1]);
         }
 
-         for (int i = 0; i < m - 1; i++) {
-            if(prefix[i][n-1] == suffix[i+1][0]){
+        for (int i = 0; i < m - 1; i++) {
+            if (total - prefixH[i] == prefixH[i]) {
                 return true;
             }
         }
 
-        for(int j = 0 ; j < n-1 ; j++){
-            if(prefix[m-1][j] == suffix[0][j+1]){
+        for (int j = 0; j < n - 1; j++) {
+            if (total - prefixV[j] == prefixV[j]) {
                 return true;
             }
         }
