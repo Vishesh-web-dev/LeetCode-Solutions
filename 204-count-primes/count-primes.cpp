b69d202574533e6a2dd99771ba24a4bc3ecc1ea6
Vishesh Shrivastava <visheshshrivastava2002@gmail.com>
2026-04-02 09:38:22 +0530

    Time: 182 ms (51.27%) | Memory: 132.6 MB (8.36%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,14 +3,25 @@ public:
     int countPrimes(int n) {
         vector<int> primes(n, 0);
         int primeCnt = 0;
+        // for(int i = 2 ; i < n ; i++){
+        //     if(primes[i] == 0){
+        //         primeCnt++;
+        //     }
+        //     int j = 2;
+        //     while((i*j) < n){
+        //         primes[i*j] = 1;
+        //         j++;
+        //     }
+        // }
+
+
+        //mark multiples of a number only when it is a prime and start marking from i square
         for(int i = 2 ; i < n ; i++){
             if(primes[i] == 0){
                 primeCnt++;
-            }
-            int j = 2;
-            while((i*j) < n){
-                primes[i*j] = 1;
-                j++;
+                for(long long j = (long long)i*i ; j< n ; j+=i){
+                    primes[j] = 1;
+                }
             }
         }
         return primeCnt;
